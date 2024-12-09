c1e1d8aa998f8320a89f44c6cc73117588bd573d
root <ahmadmodallal87@gmail.com>
2024-12-09 12:37:50 +0000

    add

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,16 @@
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-#include <stdio.h>
+
 
 int main(void)
 {
 	/* Print*/
- puts("\"Programming is like building a multilingual puzzle");
+	 puts("\"Programming is like building a multilingual puzzle");
  	/*Return 0 to indicate the program executed successfully*/
- return (0);
+	 return (0);
 }
