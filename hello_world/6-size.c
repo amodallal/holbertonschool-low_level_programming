1d6aba9d733bbf61319ebc6b46c1ca4ed6cebee8
root <ahmadmodallal87@gmail.com>
2024-12-09 19:44:08 +0000

    add

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,9 +14,9 @@ int main(void)
 	/* Print*/
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("Size of long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %zu byte(s)\n", sizeof(long long));
-	printf("Size of float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	/*Return zero to indicate the program executed successfully*/
 	return (0);
 }
