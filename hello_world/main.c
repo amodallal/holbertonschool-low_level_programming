d543b4e25ad08a8791a109f98f6891dd0e78541c
root <ahmadmodallal87@gmail.com>
2024-12-09 09:22:08 +0000

    update

--- hello_world/main.c
+++ hello_world/main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-#include <stdio.h>
-
-int main() {
-  printf("Hello World!");
-  return 0;
+int main(void)
+{
+    return (0);
 }
