2b0605a2a68daf20aa3eb12a7fedd9e1d6379d29
root <ahmadmodallal87@gmail.com>
2024-12-09 19:40:33 +0000

    add

---
+++ hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*
+*/
+
+
+
+int main(void)
+{
+	/* Print*/
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(long long));
+	printf("Size of float: %zu byte(s)\n", sizeof(float));
+	/*Return zero to indicate the program executed successfully*/
+	return (0);
+}
