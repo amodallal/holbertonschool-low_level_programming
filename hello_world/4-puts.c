baa30559c30e335ca9220270ee45edadc64ca7d6
root <ahmadmodallal87@gmail.com>
2024-12-09 12:35:25 +0000

    add

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	/* Print*/
- puts("\"Programming is like building a multilingual puzzle"); 
+ puts("\"Programming is like building a multilingual puzzle");
  	/*Return 0 to indicate the program executed successfully*/
  return (0);
 }
