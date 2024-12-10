7848c13ac655a5ab086a9a01671b36aa3b5cbab3
root <ahmadmodallal87@gmail.com>
2024-12-10 17:45:32 +0000

    add

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,8 +5,13 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-	print_alphabet();
-	return (0);
+void print_alphabet(void)
+	{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	_putchar(letter);
+	}
+	_putchar('\n');
 }
