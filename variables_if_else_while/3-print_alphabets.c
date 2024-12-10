d9821072a52c852471c2e7a82788b18422e72d7a
root <ahmadmodallal87@gmail.com>
2024-12-10 09:37:39 +0000

    add

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+	putchar(letter);
+	}
+	putchar('\n'); /* Print a newline after the alphabet */
+	}
+
+/**
+ * main - Entry point, calls print_alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
