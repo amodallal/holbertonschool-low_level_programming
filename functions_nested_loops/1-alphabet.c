11765a913b31503c1592bb80422f3e2257893227
root <ahmadmodallal87@gmail.com>
2024-12-10 17:19:07 +0000

    add

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Print*/
+	print_alphabet();
+	/*Return zero to indicate the program executed successfully*/
+	return (0);
+}
--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -8,3 +8,12 @@
 	int _putchar(char c) {
 	return write(1, &c, 1);
 	}
+
+void print_alphabet(void) {
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++) {
+        _putchar(letter);  
+	 }
+	 _putchar('\n'); 
+} 
