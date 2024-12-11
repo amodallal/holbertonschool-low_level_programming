822c2c9760f659399f39c36eab903981f441c6a7
root <you@example.com>
2024-12-11 12:17:36 +0000

    add

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * times_table - Checks if a character is lowercase.
+ *
+ *
+ * Description: times table
+ *
+ *
+ *
+ * Return: times table
+ */
+
+void times_table(void)
+{
+
+}
