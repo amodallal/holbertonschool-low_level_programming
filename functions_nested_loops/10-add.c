6f933aae5f5bfdf7f812ac8aa2dd75917213f236
root <you@example.com>
2024-12-11 16:48:54 +0000

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ *
+ * Description: This function adds two numbers
+ * @i: The first integer to be added.
+ * @j: The second integer to be added.
+ *
+ * Return: return result
+ */
+int add(int i, int j)
+{
+	int n = i + j;
+
+	return (n);
+}
--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ void times_table(void)
 	{
 	for (j = 0; j <= 9; j++) /* Loop through columns */
 	{
-	result = i * j;
+	resulit = i * j;
 
 	if (result < 10 && j != 0)
 	_putchar(' '); /* Padding for single-digit numbers */
