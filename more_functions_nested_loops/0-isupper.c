6805d99358a7d49666e2c394eead008ec33ea991
root <you@example.com>
2024-12-11 19:03:39 +0000

    add

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+/**
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ * Return: On success, 1 is returned. On error, -1 is returned.
+ */
+int _isupper(int c){
+
+} 
