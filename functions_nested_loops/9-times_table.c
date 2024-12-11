1c185c6bf2da01fc50a0daca6642d19015edcd9b
root <you@example.com>
2024-12-11 16:18:52 +0000

    add

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -2,9 +2,7 @@
 /**
  * times_table - Checks if a character is lowercase.
  *
- *
  * Description: times table
- *
  *
  *
  * Return: times table
@@ -15,27 +13,25 @@ void times_table(void)
 	int i;
 	int j;
 	int result;
+
 	for (i = 0 ; i <= 9 ; i++)
 	{
-		for (j = 0 ; j <= 9 ;j++)
+		for (j = 0 ; j <= 9 ; j++)
 		{
 			result = i * j;
 			if (result >= 10)
 
-				{
+			{
 					_putchar('0' + (result / 10));
 					_putchar('0' + (result % 10));
-                        		_putchar(',');
-                        		_putchar(' ');
-					
-				
-				}
-			else {
+					_putchar(',');
+					_putchar(' ');
+			}
+			else
+			{
 					_putchar('0' + result);
 					_putchar(',');
-					_putchar(' ');	
-				
-					
+					_putchar(' ');
 			}
 		}
 		_putchar('\n');
