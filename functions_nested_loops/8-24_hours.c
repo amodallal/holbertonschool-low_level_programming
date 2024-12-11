57060191e203de7183dd13fa0aa655121050ad31
root <you@example.com>
2024-12-11 11:09:11 +0000

    add

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * jack_bauer - print every minute
+ * 
+ *
+ * Description: This function prints ebery minute
+ *
+ *
+ *
+ * Return
+ */
+void jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	for (hour = 0 ; hour < 24 ; hour++)
+	{
+		for(minute = 0 ; minute < 60 ; minute++)
+		{
+			_putchar('0' + hour / 10);
+			_putchar('0' + hour % 10);
+		       	_putchar(':');
+
+			_putchar('0' + minute / 10);
+			_putchar('0' + minute % 10);
+			_putchar('\n');	
+		}
+	}
+
+}
