f394d415896c7c8cc670390d4e2a4f0391a43d27
root <you@example.com>
2024-12-11 16:12:22 +0000

    add

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,14 +15,29 @@ void times_table(void)
 	int i;
 	int j;
 	int result;
-	for (i = 0 ; i <= 10 ; i++)
+	for (i = 0 ; i <= 9 ; i++)
 	{
-		for (j = 0 ; j <=10 ;j++)
+		for (j = 0 ; j <= 9 ;j++)
 		{
 			result = i * j;
-			_putchar('0' + result);
-                        _putchar(',');
-                        _putchar(' ');
+			if (result >= 10)
+
+				{
+					_putchar('0' + (result / 10));
+					_putchar('0' + (result % 10));
+                        		_putchar(',');
+                        		_putchar(' ');
+					
+				
+				}
+			else {
+					_putchar('0' + result);
+					_putchar(',');
+					_putchar(' ');	
+				
+					
+			}
 		}
+		_putchar('\n');
 	}
 }
