83e8ab4ce0b9a5aedd574e96f1e02023fde99504
root <you@example.com>
2024-12-11 09:53:10 +0000

    add

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _abs - Checks if a character is lowercase.
+ * @n: The character to check.
+ *
+ * Description: This function checks if the input character `c`
+ * computes the absolute value of an integer
+ *
+ *
+ * Return: 1 if `c` is lowercase, 0 otherwise.
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		n = n;
+	}
+	else if (n == 0)
+	{
+		n = 0;
+	}
+	else
+	{
+		n = n * -1;
+	}
+return (n);	
+}
