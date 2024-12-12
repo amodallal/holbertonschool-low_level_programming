ffbeb4d1f5fe3c73bbb8931c3cd97dcf6a0f3eb9
root <ahmadmodallal87@gmail.com>
2024-12-12 11:35:09 +0000

    add

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * print_diagonal - print diagonal
+ *
+ *
+ * Return: return the printed 
+ */
+void print_diagonal(int n)
+{
+	
+}
