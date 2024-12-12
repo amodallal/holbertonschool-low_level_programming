0a1db9eaea1eaaff91d5b8b99995cf4ca8b3e153
root <ahmadmodallal87@gmail.com>
2024-12-12 17:27:10 +0000

    add

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,19 +12,18 @@ void more_numbers(void)
 	int i ;
 	int j ;
 
-	for (i = 0 ; i <= 10 ; i++)
+	for (i = 0 ; i <10 ; i++)
 	{	
 		for (j = 0 ; j <=14 ; j++)
 		{
-			if(j >= 10)
+			if(j > 9)
 			{
-				_putchar('1');
-				_putchar(j % 10 + '0');
+				_putchar('0' + (j / 10));	
 			}
-			else
-			{
-			_putchar('0' + j);
-			}
+			
+			
+				_putchar('0' + (j % 10));
+			
 		}
 
 		_putchar('\n');
