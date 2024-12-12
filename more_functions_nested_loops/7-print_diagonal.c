166bf9567974a9d0c3448223572664c91b24c9a5
root <ahmadmodallal87@gmail.com>
2024-12-12 12:16:10 +0000

    add

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,5 +9,25 @@
  */
 void print_diagonal(int n)
 {
-	
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0 ; i <= n ; i++)
+		{
+			for (j = 0 ; j <=n ; j++)
+			{
+				_putchar(' ');
+				
+			}
+			_putchar(' ');
+			_putchar('\n');
+		}
+
+	}	
 }
