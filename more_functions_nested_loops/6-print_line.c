ee015e27193d5cf70767e8cbf0bc27a16292bb6c
root <ahmadmodallal87@gmail.com>
2024-12-12 11:15:57 +0000

    add

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * print_line - print line
+ * @n: 
+ *
+ * Return: return line
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	for (i = 0 ; i >= n ; i++)
+	{
+		_putchar('_');
+	}
+	}
+}
