8a23620d40b3da0605cbe8e7084a83059c45919a
root <ahmadmodallal87@gmail.com>
2024-12-13 07:34:30 +0000

    add

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * print_ - print square
+ *
+ * @size: variable
+ * Return: return the printed
+ */
+int main(void)
+{
+	int i ;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		if ((i % 5 == 0) && (i % 3 == 0 ))
+                {
+                        printf("FizzBuzz");
+                }
+		else if (i % 3  == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+
+		else
+		{
+			printf("%d",i);
+		}
+		printf(" ");
+	}
+	printf("\n");
+	return (0) ;
+}
