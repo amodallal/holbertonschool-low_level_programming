c5c2e8af22a4e312346dc0474bfa1c7043b4834e
root <ahmadmodallal87@gmail.com>
2024-12-16 11:37:02 +0000

    add

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * print_rev - print string
+ *
+ * @s: variable
+ *
+ * Return: return the printed
+ */
+void print_rev(char *s)
+{
+	int lenght = 
+
+}
