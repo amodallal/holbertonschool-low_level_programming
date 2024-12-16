28fea07fc841b67a705d44c624ae9ea0f5738165
root <ahmadmodallal87@gmail.com>
2024-12-16 19:08:05 +0000

    add

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * puts2 - print string
+ *
+ * @str: variable
+ *
+ * Return: return the printed
+ */
+void puts2(char *str)
+{
+	int len = 0, i;
+	
+	while (str[len] != '\0')
+	len++;
+	for (i = 0 ; i <= len ; i = i + 2)
+	{
+		
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
