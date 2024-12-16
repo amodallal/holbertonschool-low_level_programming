ca3f0f90efb881dc6ca090df5e4ba1ebc36324fc
root <ahmadmodallal87@gmail.com>
2024-12-16 11:50:24 +0000

    add

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,6 +10,17 @@
  */
 void print_rev(char *s)
 {
-	int lenght = 
+	int length = 0;
+	int i;
 
+	while (s[length] != '\0')
+	{
+	length++;
+	}
+
+	for (i = length ; i >= 0 ; i--)
+	{
+	_putchar(s[i]);
+	}
+	_putchar('\n');
 }
