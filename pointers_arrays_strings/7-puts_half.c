7231a273d19bd7ca5bcf6ecb3f981bb8b7a134ef
root <ahmadmodallal87@gmail.com>
2024-12-16 19:53:05 +0000

    add

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,24 +10,15 @@
  */
 void puts_half(char *str)
 {
-	int len = 0;
-	int i;
-	int n = 0;
+	int len = 0, start, i;
 
 	while (str[len] != '\0')
 	len++;
-	if (len % 2 != 0)
-	{
-		n = ((len - 1) / 2);
-	for (i = 0 ; i <= n ; i++)
-	_putchar(str[i]);
-	}
+	if (len % 2 == 0)
+	start = len / 2;
 	else
-	{
-	for (i = (len / 2) ; i <= (len) ; i++)
-	{
+	start = (len - 1) / 2 + 1;
+	for (i = start; i < len; i++)
 	_putchar(str[i]);
-	}
-	}
 	_putchar('\n');
 }
