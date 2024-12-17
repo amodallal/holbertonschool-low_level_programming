b3c8fdf4840661ef3527ef29aaac34f7ad5c042b
root <ahmadmodallal87@gmail.com>
2024-12-17 09:53:03 +0000

    add

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,25 @@
 #include "main.h"
-#include <unistd.h>
-#include <stdio.h>
+
 /**
- * _strcat - strcat
+ * _strncat - Concatenates two strings up to n characters
+ * @dest: The destination string
+ * @src: The source string to append to dest
+ * @n: The maximum number of characters to append
  *
- * @src: variable
- * @dest: variable
- * Return: return the printed
+ * Return: Pointer to the resulting string dest
  */
-char *_strncat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, dest_len = 0; int src_len = 0;
+	int i = 0, dest_len = 0;
 
 	while (dest[dest_len] != '\0')
-        dest_len++;
-	
-	while (src[src_len]) != '\0')
-	src_len++
-	for (i = )	
+	dest_len++;
+	while (i < n && src[i] != '\0')
+	{
+	dest[dest_len + i] = src[i];
+	i++;
+	}
+dest[dest_len + i] = '\0';
+return (dest);
 }
 
