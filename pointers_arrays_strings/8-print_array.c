0b2091af5723ecc4225e80c4b86ef329c5e78e32
root <ahmadmodallal87@gmail.com>
2024-12-17 06:55:11 +0000

    add

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * print_array - print hlaf string
+ *
+ * @n: variable
+ * @a: variable
+ * Return: return the printed
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i <= n ; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n)
+		{
+		printf(", ");
+		}
+	}
+	printf("\n");
+}
