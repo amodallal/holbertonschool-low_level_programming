9d898ffe563e9c457155c3f529f9c8f83a1846f8
root <ahmadmodallal87@gmail.com>
2025-01-09 10:48:10 +0000

    add

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -39,7 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < len1; i++)
 	concat[i] = s1[i];
 	for (i = 0; i < n; i++)
- 	concat[len1 + i] = s2[i];
+	concat[len1 + i] = s2[i];
 	concat[len1 + n] = '\0';
 	return (concat);
 }
