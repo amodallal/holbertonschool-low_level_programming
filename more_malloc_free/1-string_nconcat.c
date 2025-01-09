6da55c895cbc2a6cee17b3558383b86276457a79
root <ahmadmodallal87@gmail.com>
2025-01-09 10:42:04 +0000

    add

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,20 @@
 #include <stdlib.h>
-#include <string.h>
+
+/**
+ *  * _strlen - Calculates the length of a string.
+ *   * @str: The string to measure.
+ *    *
+ *     * Return: The length of the string.
+ *      */
+unsigned int _strlen(char *str)
+{
+	    unsigned int len = 0;
+
+	        while (str[len] != '\0')
+			        len++;
+
+		    return (len);
+}
 
 /**
  * string_nconcat - Concatenates two strings up to n bytes from the second string.
@@ -7,6 +22,7 @@
  * @s2: The second string.
  * @n: The number of bytes to concatenate from s2.
  * Return: A pointer to the newly allocated space containing s1 followed
+ * by the first n bytes of s2, or NULL if the function fails.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
@@ -17,17 +33,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	s1 = "";
 	if (s2 == NULL)
 	s2 = "";
-	len1 = strlen(s1);
-	len2 = strlen(s2);  
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 	if (n >= len2)
 	n = len2;
-	concat = malloc(len1 + n + 1); 
+	concat = malloc(len1 + n + 1);
 	if (concat == NULL)
 	return (NULL);
 	for (i = 0; i < len1; i++)
 	concat[i] = s1[i];
 	for (i = 0; i < n; i++)
-	concat[len1 + i] = s2[i];
+ 	concat[len1 + i] = s2[i];
 	concat[len1 + n] = '\0';
 	return (concat);
 }
