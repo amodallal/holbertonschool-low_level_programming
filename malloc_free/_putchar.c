d95f71726f50b98a22d1ea119c82b866f4dd5842
root <ahmadmodallal87@gmail.com>
2025-01-09 07:41:06 +0000

    add

---
+++ malloc_free/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ * Return: On success, 1 is returned. On error, -1 is returned.
+ */
+
+	int _putchar(char c) {
+	return write(1, &c, 1);
+	} 
