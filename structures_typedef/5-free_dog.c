b14e7db92fc371e959f36e90fa8edcf0ddfeb379
root <ahmadmodallal87@gmail.com>
2025-01-14 12:56:45 +0000

    add

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+void free_dog(dog_t *d)
+{
+    if (d != NULL)
+    {
+        if (d->name != NULL)
+        {
+            free(d->name);
+        }
+
+        if (d->owner != NULL)
+        {
+            free(d->owner);
+        }
+
+        free(d);
+    }
+}
+
