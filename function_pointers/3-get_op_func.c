bf070cf3a5ec60b8c30903cc0537e78ddeb9192d
root <ahmadmodallal87@gmail.com>
2025-01-21 07:51:26 +0000

    add

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -8,23 +8,23 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
-	};
-	int i = 0;
+    op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
 
-	if (s == NULL || s[1] != '\0')
-	return (NULL);
-	while (ops[i].op != NULL)
-	{
-	if (*(ops[i].op) == *s)
-	return (ops[i].f);
-	i++;
-	}
-	return (NULL);
+    int i = 0;
+
+    while (ops[i].op != NULL)
+    {
+        if (*(ops[i].op) == *s)
+            return (ops[i].f);
+        i++;
+    }
+
+    return (NULL);
 }
