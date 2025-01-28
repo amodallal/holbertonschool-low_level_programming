8e01887c1e15ef4d8cae21ed7c3bcbe6605be617
root <ahmadmodallal87@gmail.com>
2025-01-28 08:33:23 +0000

    add

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -9,14 +9,20 @@
  * @len: Length of the string
  * @next: Points to the next node
  *
- * Description: Singly linked list node structure
+ * Description: Node structure for singly linked list project.
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
+
+/* Function Prototypes */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
 #endif /* LISTS_H */
