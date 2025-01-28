2e5f38d7e9351228598874ece59bc46180dbf94f
root <ahmadmodallal87@gmail.com>
2025-01-28 09:04:56 +0000

    add

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -2,10 +2,12 @@
 #define LISTS_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dlistint_s - Doubly linked list node
- * @n: Integer stored in the node
+ * @n: Integer value stored in the node
  * @prev: Pointer to the previous node
  * @next: Pointer to the next node
  *
@@ -13,9 +15,17 @@
  */
 typedef struct dlistint_s
 {
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
-}	dlistint_t;
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+/* Function Prototypes */
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
 #endif /* LISTS_H */
