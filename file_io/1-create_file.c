4f79786bed5d2ee458a157e3ab6bc9d56b0ebb37
root <ahmadmodallal87@gmail.com>
2025-01-30 14:30:30 +0000

    add

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,34 +11,27 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, len = 0, write_status;
+	int fd, len = 0, write_status;
 
-    if (!filename) /* Check if filename is NULL */
-        return (-1);
-
-    if (text_content)
-    {
-        /* Calculate the length of text_content */
-        while (text_content[len])
-            len++;
-    }
-
-    /* Open the file with rw------- permissions, creating or truncating it */
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1) /* Check if the file could not be created/opened */
-        return (-1);
-
-    /* Write text_content to the file if it's not NULL */
-    if (text_content)
-    {
-        write_status = write(fd, text_content, len);
-        if (write_status == -1) /* Check if write failed */
-        {
-            close(fd);
-            return (-1);
-        }
-    }
-
-    close(fd); /* Close the file */
-	return (1); /* Return success */
+	if (!filename)
+	return (-1);
+	if (text_content)
+	{
+	while (text_content[len])
+	len++;
+	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+	return (-1);
+	if (text_content)
+	{
+	write_status = write(fd, text_content, len);
+	if (write_status == -1)
+	{
+	close(fd);
+	return (-1);
+	}
+	}
+	close(fd);
+	return (1);
 }
