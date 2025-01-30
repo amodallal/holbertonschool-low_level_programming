cdb0016528693406a632fa4e089f00fee4cbbf1b
root <ahmadmodallal87@gmail.com>
2025-01-30 14:09:31 +0000

    add

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,14 +4,14 @@
 #include <stdio.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and print it
  * @filename: name of the file to read.
  * @letters: number of letters to read and print.
  *
  * Return: the actual number of letters it could read and print.
  *         If the file cannot be opened or read, return 0.
  *         If filename is NULL, return 0.
- *         If write fails or does not write the expected amount of bytes, return 0.
+ *         If write fails or does not write the expected amo
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -28,7 +28,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buffer == NULL)
 	{
 	close(fd);
- 	return (0);
+	return (0);
 	}
 	bytesRead = read(fd, buffer, letters);
 	if (bytesRead == -1)
