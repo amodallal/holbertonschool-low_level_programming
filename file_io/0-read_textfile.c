ab65b65a905a9ef840311fee4fbfde9ca1e2252f
root <ahmadmodallal87@gmail.com>
2025-01-30 14:07:51 +0000

    add

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,44 +3,48 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * @filename: name of the file to read.
+ * @letters: number of letters to read and print.
+ *
+ * Return: the actual number of letters it could read and print.
+ *         If the file cannot be opened or read, return 0.
+ *         If filename is NULL, return 0.
+ *         If write fails or does not write the expected amount of bytes, return 0.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t bytesRead, bytesWritten;
-    char *buffer;
+	int fd;
+	ssize_t bytesRead, bytesWritten;
+	char *buffer;
 
-    if (filename == NULL)
-        return (0);
-
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
-
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(fd);
-        return (0);
-    }
-
-    bytesRead = read(fd, buffer, letters);
-    if (bytesRead == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
-
-    bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
-    if (bytesWritten == -1 || bytesWritten != bytesRead)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
-
-    free(buffer);
-    close(fd);
-
+	if (filename == NULL)
+	return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+	close(fd);
+ 	return (0);
+	}
+	bytesRead = read(fd, buffer, letters);
+	if (bytesRead == -1)
+	{
+	free(buffer);
+	close(fd);
+	return (0);
+	}
+	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+	if (bytesWritten == -1 || bytesWritten != bytesRead)
+	{
+	free(buffer);
+	close(fd);
+	return (0);
+	}
+	free(buffer);
+	close(fd);
 	return (bytesRead);
 }
